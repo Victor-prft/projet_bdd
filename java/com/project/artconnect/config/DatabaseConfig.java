e13de7936afe80e0272a6c91f7a31fdbe91e88d2
vico <victor.proffit@efrei.net>
2026-05-18 01:37:29 +0200

    utf 8 corrigé

--- java/com/project/artconnect/config/DatabaseConfig.java
+++ java/com/project/artconnect/config/DatabaseConfig.java
@@ -1,7 +1,7 @@
 package com.project.artconnect.config;
 
 public class DatabaseConfig {
-    public static final String URL      = "jdbc:mysql://localhost:3306/artconnect";
+    public static final String URL      = "jdbc:mysql://localhost:3306/artconnect?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Europe/Paris";
     public static final String USER     = "root";
     public static final String PASSWORD = "Bayana15-";
 }
